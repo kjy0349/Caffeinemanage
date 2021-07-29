3d7c316aa7e8fecd5a2bd782ebeee94795270178
DESKTOP-OVK9G3S\kjy03 <kjy0349@naver.com>
2021-07-29 17:59:23 +0900

    Navigation bar added

--- app/src/main/java/com/example/caffeinemanage/MainActivity.kt
+++ app/src/main/java/com/example/caffeinemanage/MainActivity.kt
@@ -1,17 +1,68 @@
 package com.example.caffeinemanage
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationMenu
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    fun setCaffeine(mg: Int, cf_result: TextView){
+        var cf_str = String.format("%d mg", mg)
+        cf_result.text = cf_str
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val leap = findViewById(R.id.leap) as ImageView
+
+        Glide.with(this).load(R.raw.updownleap).override(1000, 1000).into(leap)
+
+
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.page_1 -> {
+                    // Respond to navigation item 1 click
+                    true
+                }
+                R.id.page_2 -> {
+                    // Respond to navigation item 2 click
+                    true
+                }
+                R.id.page_3 -> {
+                    // Respond to navigation item 2 click
+                    true
+                }
+                R.id.page_3 -> {
+                    // Respond to navigation item 2 click
+                    true
+                }
+
+                else -> false
+            }
+        }
+        BottomNavigationView.OnNavigationItemReselectedListener { item ->
+            when(item.itemId) {
+                R.id.page_1 -> {
+                    // Respond to navigation item 1 reselection
+                }
+                R.id.page_2 -> {
+                    // Respond to navigation item 1 reselection
+                }
+                R.id.page_3 -> {
+                    // Respond to navigation item 1 reselection
+                }
+                R.id.page_4 -> {
+                    // Respond to navigation item 1 reselection
+                }
+            }
+        }
 
     }
 
